7bd60e70ea79fc2384af70e1978a50d0be35467e
Anatol Derksen <derksen@fs-net.de>
2024-10-28 09:22:09 +0100

    Remove scrips directory
    Remove scrips directory. fus-setup-release.sh script appends
    additional layer like meta-fus

--- scripts/fus_setup.sh
+++
@@ -1,5 +0,0 @@
-# Setup script to initialize the layer.
-# This script will be called by fus-setup-release.sh which is located in the yocto root folder
-
-# Add layer to bblayers.conf
-echo "BBLAYERS += \" \${BSPDIR}/sources/meta-fus-updater-azure \"" >> $BUILD_DIR/conf/bblayers.conf
